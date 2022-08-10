d05b74cbfe282a972ce835bb065aeee8aa36cdd3
oleg <opa@exante.eu>
2022-08-10 18:16:06 +0300

    Draft.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,51 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Redirect, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+
+type TRenderData = {
+    isStatusPage?: true;
+    isLogsPage?: true;
+    isAddTaskPage?: true;
+    isShutdownPage?: true;
+};
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get('/')
+    @Post('/')
+    @Render('status')
+    async getStatusPage(): Promise<TRenderData> {
+        return { isStatusPage: true };
+    }
+
+    @Get('/logs')
+    @Render('logs')
+    async getLogsPage(): Promise<TRenderData> {
+        return { isLogsPage: true };
+    }
+
+    @Get('/add-task')
+    @Render('add-task')
+    async getAddTaskPage(): Promise<TRenderData> {
+        return { isAddTaskPage: true };
+    }
+
+    @Post('/add-task')
+    @Redirect('/')
+    async addTask(): Promise<void> {
+        return;
+    }
+
+    @Get('/shutdown')
+    @Render('shutdown')
+    async getAddTask(): Promise<TRenderData> {
+        return { isShutdownPage: true };
+    }
+
+    @Post('/shutdown')
+    @Redirect('/')
+    async shutdown(): Promise<void> {
+        await this.appService.shutdown();
+    }
 }
